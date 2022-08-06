4c44734e3d9e937323ef80384f7076da7ca21bd7
Pepp <reels-fencers.0@icloud.com>
2022-08-06 01:06:55 -0400

    add event listener to buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmmitHandler = function(event) {
     event.preventDefault();
@@ -101,5 +102,35 @@ var getUserRepos = function(user) {
     });
 };    
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: Github User Not Found');
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+
+}
+
 userFormEl.addEventListener("submit", formSubmmitHandler);
 // getUserRepos("user");
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
